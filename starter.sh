b6f510faaeecc465d0aa89c46aaccb403429bf8a
Thomas Ntinas <86719110+thomasnti@users.noreply.github.com>
2023-04-11 10:03:29 +0300

    change starter.sh

--- starter.sh
+++ starter.sh
@@ -4,6 +4,10 @@ git submodule update --init --recursive
 
 git submodule foreach --recursive flutter pub get
 
+git submodule foreach --recursive dart run husky install
+
+dart run husky install
+
 flutter pub get
 
 echo "Ready to GO!"
